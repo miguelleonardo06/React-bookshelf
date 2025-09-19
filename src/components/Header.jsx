e35d3de9382da76a74dfa4e13be0298af72e9e95
Miguel <miguelleonardo.basc@gmail.com>
2025-09-19 14:21:30 +0800

    complete navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,12 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const pages = ["TRENDING", "BROWSE", "RANDOM", "ABOUT"];
 
 export function Header() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -73,6 +71,9 @@ export function Header() {
               ))}
             </Menu>
           </Box>
+          <MenuBookIcon
+            sx={{ marginRight: "5px", display: { xs: "none", md: "flex" } }}
+          />
           <Typography
             variant="h5"
             noWrap
@@ -91,7 +92,29 @@ export function Header() {
           >
             BOOKSHELF
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex", alignItems: "center" },
+            }}
+          >
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              BOOKSHELF
+            </Typography>
             {pages.map((page) => (
               <Button
                 key={page}
@@ -102,6 +125,7 @@ export function Header() {
               </Button>
             ))}
           </Box>
+
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings"></Tooltip>
             <Menu
@@ -119,15 +143,7 @@ export function Header() {
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            ></Menu>
           </Box>
         </Toolbar>
       </Container>
