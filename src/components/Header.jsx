7d974fff56dbc3062c86e3a8f2529b9a2b3fe19a
Miguel <miguelleonardo.basc@gmail.com>
2025-09-19 15:12:53 +0800

    new title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -113,7 +113,7 @@ export function Header() {
                 textDecoration: "none",
               }}
             >
-              BOOKSHELF
+              BOOK
             </Typography>
             {pages.map((page) => (
               <Button
