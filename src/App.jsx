57b2531e695d9468449c677698a337bb082de3ff
Miguel <miguelleonardo.basc@gmail.com>
2025-09-19 14:06:02 +0800

    boiler plate of bookshelf

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header";
+import Box from "@mui/material/Box";
+
+export function App() {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
